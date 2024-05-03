b9b2fab0aa3c906f8d791d2a1be6ad43c507b710
aurelggd <aurelgbaguidi@gmail.com>
2024-05-03 14:46:03 +0200

    Project setting up, Challenge and ChallengeController classes

---
+++ src/main/java/com/challenge/ChallengeApp/ChallengeController.java
@@ -0,0 +1,12 @@
+package com.challenge.ChallengeApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChallengeController {
+    private List<Challenge> challenges = new ArrayList<>();
+
+    public List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+}
