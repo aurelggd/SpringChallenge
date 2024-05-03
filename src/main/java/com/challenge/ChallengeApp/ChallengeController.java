e145458ce9e4e2402155b52df67df47384afb215
aurelggd <aurelgbaguidi@gmail.com>
2024-05-03 16:34:28 +0200

    challenge1 added to challenges list

--- src/main/java/com/challenge/ChallengeApp/ChallengeController.java
+++ src/main/java/com/challenge/ChallengeApp/ChallengeController.java
@@ -1,11 +1,21 @@
 package com.challenge.ChallengeApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class ChallengeController {
     private List<Challenge> challenges = new ArrayList<>();
 
+    public ChallengeController() {
+        Challenge challenge1 = new Challenge(1L, "January", "Learn a new language");
+        challenges.add(challenge1);
+    }
+
+    @GetMapping("/challenges")
     public List<Challenge> getAllChallenges() {
         return challenges;
     }
