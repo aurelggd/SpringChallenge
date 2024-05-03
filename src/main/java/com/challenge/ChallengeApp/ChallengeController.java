5b86a9cfd3d6337959da0893a7f763efd7072177
aurelggd <aurelgbaguidi@gmail.com>
2024-05-03 17:14:46 +0200

    addChallenge methode

--- src/main/java/com/challenge/ChallengeApp/ChallengeController.java
+++ src/main/java/com/challenge/ChallengeApp/ChallengeController.java
@@ -1,6 +1,8 @@
 package com.challenge.ChallengeApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,5 +20,11 @@ public class ChallengeController {
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges() {
         return challenges;
+    }
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge) {
+        challenges.add(challenge);
+        return "Challenge added successfully";
     }
 }
